074f5cd5e70afccd0d362c16517047610d75265e
Ramos Hugo <hugoramos1466@gmail.com>
2026-03-04 09:21:07 +0000

    Remove merge conflict markers and unused imports in __init__.py

--- part2/app/api/v1/__init__.py
+++ part2/app/api/v1/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Blueprint
 from flask_restx import Api
 
@@ -19,11 +18,3 @@ api.add_namespace(users_ns, path="/users")
 api.add_namespace(amenities_ns, path="/amenities")
 api.add_namespace(places_ns, path="/places")
 api.add_namespace(reviews_ns, path="/reviews")
-=======
-from app.api.v1.users import api as users_ns
-from app.api.v1.amenities import api as amenities_ns
-from app.api.v1.places import api as places_ns
-from app.api.v1.reviews import api as reviews_ns
-
-__all__ = ["users_ns", "amenities_ns", "places_ns", "reviews_ns"]
->>>>>>> origin/dev
