52af2f27f42de936aa14194a7756b509cad8e321
Ramos Hugo <hugoramos1466@gmail.com>
2026-03-19 11:05:44 +0100

    fix: remove invalid characters and ensure proper file termination in amenity.py

--- part3/app/models/amenity.py
+++ part3/app/models/amenity.py
@@ -50,3 +50,4 @@ class Amenity(BaseModel):
             'description': self.description
         })
         return base
+drbgergidsbgiumeigumeiugberiubgkierbgierbigeriubgiuerbgieiugbaeriumgniuerniudfiugeihgierniueriugerifumoerhiueriu
